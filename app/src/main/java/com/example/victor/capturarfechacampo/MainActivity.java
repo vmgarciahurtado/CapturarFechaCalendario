81c41a306b38631c680d7486196ddbbfa80e5320
victor <vmgarciahurtado@gmail.com>
2019-04-23 09:59:33 -0500

    SE creo el proyecto

--- app/src/main/java/com/example/victor/capturarfechacampo/MainActivity.java
+++ app/src/main/java/com/example/victor/capturarfechacampo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.victor.capturarfechacampo;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import java.util.Calendar;
@@ -14,6 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.campoCalendario);
+        campoCalendario = findViewById(R.id.campoCalendario);
+        campoCalendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentDate = Calendar.getInstance();
+                DatePickerDialog mDatePicker = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                                campoCalendario.setText(""+year+"/"+month+"/"+dayOfMonth);
+                            }
+                        },mcurrentDate.get(Calendar.YEAR),mcurrentDate.get(Calendar.MONTH),mcurrentDate.get(Calendar.DAY_OF_MONTH));
+                mDatePicker.setTitle("SELET DATE");
+                mDatePicker.show();
+            }
+        });
     }
 }
